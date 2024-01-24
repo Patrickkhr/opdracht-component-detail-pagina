6745246bf06438e1b774d936e37c6ee39bd446de
Patrickkhr <patrickhrdjk@gmail.com>
2024-01-24 23:26:06 +0100

    comments js

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,9 @@ document.addEventListener('DOMContentLoaded', function() {
   var tabButtons = document.querySelectorAll('.tablinks');
   tabButtons.forEach(function(button) {
     button.addEventListener('click', function(event) {
+      // Get the tab name from the data-tab attribute of the button
       var tabName = button.getAttribute('data-tab');
+      // Call the openTab function to display the selected tab
       openTab(event, tabName);
     });
   });
@@ -11,7 +13,9 @@ document.addEventListener('DOMContentLoaded', function() {
   // Add event listener to select element
   var tabSelector = document.getElementById('tabSelector');
   tabSelector.addEventListener('change', function(event) {
+    // Get the selected value from the select element
     var tabName = event.target.value;
+    // Call the openTab function to display the selected tab
     openTab(event, tabName);
   });
   
@@ -19,25 +23,34 @@ document.addEventListener('DOMContentLoaded', function() {
   var copyButtons = document.querySelectorAll('.copyButton');
   copyButtons.forEach(function(copyButton) {
     copyButton.addEventListener('click', function(event) {
+      // Get the ID of the tab where the copy button is located
       var tabId = copyButton.closest('.tabcontent').id;
+      // Check if the tab is a codeTab
       var isCodeTab = copyButton.closest('.codeTab') !== null;
+      // Build the selector based on the tab and whether it's a codeTab
       var selector = '#' + tabId + (isCodeTab ? ' .codeTab code' : ' .npmTab .npmCopy');
+      // Call the copyToClipboard function to copy text to the clipboard
       copyToClipboard(selector, copyButton);
     });
   });
 });
 
+// Function to display the selected tab
 function openTab(evt, tabName) {
   var i, tabscontent, tablinks;
+  // Hide all tab content
   tabscontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabscontent.length; i++) {
     tabscontent[i].style.display = "none";
   }
+  // Remove the 'active' class from all tab buttons
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
+  // Show the content of the selected tab
   document.getElementById(tabName).style.display = "block";
+  // Add the 'active' class to the selected tab button
   evt.currentTarget.className += " active";
 }
 
@@ -64,24 +77,35 @@ function copyToClipboard(selector, button) {
 }
 
 document.addEventListener("DOMContentLoaded", function() {
+  // Find the button inside the .jsonTab element
   let jsonButton = document.querySelector(".jsonTab button");
   
+  // Add a click event listener to the jsonButton
   jsonButton.addEventListener("click", function() {
+    // Find the #codeJSON element
     let codeTab = document.querySelector("#codeJSON");
+    
+    // Find the closest ancestor section of codeTab
     let section = codeTab.closest('section');
+    
+    // Get the text content of the button and convert it to uppercase
     let linkText = this.textContent.toUpperCase();
 
+    // Toggle the visibility of codeTab based on the button text
     if (linkText === "TOON ALLE CODE") {
+      // Change button text and show code content
       linkText = "Toon minder code";
       codeTab.classList.remove("hideContent");
       codeTab.classList.add("showContent");
     } else {
+      // Change button text and hide code content, scroll to section
       linkText = "Toon alle code";
       codeTab.classList.remove("showContent");
       codeTab.classList.add("hideContent");
       section.scrollIntoView({ behavior: 'smooth' });
     }
 
+    // Update the button text with the modified value
     this.textContent = linkText;
   });
 });
